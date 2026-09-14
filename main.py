ebce4bf97ed134c65c8f37ad6d2679cbf5ea38be
Atlas-The-All-Knowing <lachlan.mcgill@gmail.com>
2026-09-14 11:15:17 +0800

    build: added basic game loop

--- main.py
+++ main.py
@@ -5,10 +5,32 @@ from constants import(
     SCREEN_HEIGHT,
 )
 
+from logger import log_state
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initialises pygame
+    pygame.init()
+
+    # Sets the pygame.display size from the screen width and height constants
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        # Gets the games state and logs it
+        log_state()
+
+        # Processes events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Sets the screen to solid black, then updates it
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
