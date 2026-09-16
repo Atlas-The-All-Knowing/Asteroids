df2953acc83cdbfa7304607d490e0df9f0bf2fe7
Atlas-The-All-Knowing <lachlan.mcgill@gmail.com>
2026-09-16 10:26:42 +0800

    feat: added shooting ability for Player

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -14,16 +15,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Creates Sprite Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    shots = pygame.sprite.Group()
+
+    # Adds Sprites to various groups
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroid_field = AsteroidField()
-    dt = 0.0
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
 
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0.0
 
 
     # Game Loop
--- player.py
+++ player.py
@@ -4,9 +4,11 @@ from circleshape import CircleShape
 from constants import (
     LINE_WIDTH,
     PLAYER_RADIUS,
+    PLAYER_SHOOT_SPEED,
     PLAYER_SPEED,
     PLAYER_TURN_SPEED,
 )
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -38,6 +40,14 @@ class Player(CircleShape):
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
 
+    # Player shoots
+    def shoot(self) -> None:
+        shot = Shot(self.position.x, self.position.y)
+        unit_vector = pygame.Vector2(0,1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_velocity_vector = rotated_vector * PLAYER_SHOOT_SPEED
+        shot.velocity += rotated_with_velocity_vector
+
     # Updates the player when moved
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
@@ -50,3 +60,5 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
